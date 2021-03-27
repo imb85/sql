ea61b502c69bd0eead01ae8e42aeaf2abf01e367
Andrew Kraynak <abk89@miami.edu>
2021-03-27 17:38:31 -0400

    Corrected datatypes in insert_table_values.sql

--- hotels/insert_table_values.sql
+++ hotels/insert_table_values.sql
@@ -1,64 +1,64 @@
 -- Hotel values
 INSERT INTO Hotel (hotelNo, hotelName, city)
-VALUES ('1001', 'Holiday Inn', 'Miami');
+VALUES (1001, 'Holiday Inn', 'Miami');
 
 INSERT INTO Hotel (hotelNo, hotelName, city)
-VALUES ('1002', 'Biltmore Hotel', 'Coral Gables');
+VALUES (1002, 'Biltmore Hotel', 'Coral Gables');
 
 INSERT INTO Hotel (hotelNo, hotelName, city)
-VALUES ('1003', 'Hilton', 'Seattle');
+VALUES (1003, 'Hilton', 'Seattle');
 
 INSERT INTO Hotel (hotelNo, hotelName, city)
-VALUES ('1004', 'Double Tree', 'Chicago');
+VALUES (1004, 'Double Tree', 'Chicago');
 
 INSERT INTO Hotel(hotelNo, hotelName, city)
-VALUES ('1005', 'Quality Inn', 'Seattle');
+VALUES (1005, 'Quality Inn', 'Seattle');
 
 -- Room values
 INSERT INTO Room (roomNo, hotelNo, roomType, price)
-VALUES ('213', '1002', 'Suite', '299.99');
+VALUES (213, '1002', 'Suite', 299.99);
 
 INSERT INTO Room (roomNo, hotelNo, roomType, price)
-VALUES ('403', '1002', 'Double', '359.99');
+VALUES (403, '1002', 'Double', 359.99);
 
 INSERT INTO Room (roomNo, hotelNo, roomType, price)
-VALUES ('701', '1002', 'Penthouse', '949.99');
+VALUES (701, '1002', 'Penthouse', 949.99);
 
 INSERT INTO Room (roomNo, hotelNo, roomType, price)
-VALUES ('516', '1002', 'Double', '369.99');
+VALUES (516, '1002', 'Double', 369.99);
 
 INSERT INTO Room (roomNo, hotelNo, roomType, price)
-VALUES ('307', '1003', 'Suite', '229.99');
+VALUES (307, '1003', 'Suite', 229.99);
 
 -- Guest values
 INSERT INTO Guest (guestNo, guestName, guestAddress)
-VALUES ('1734', 'Bill Smith', 'Fort Lauderdale');
+VALUES (1734, 'Bill Smith', 'Fort Lauderdale');
 
 INSERT INTO Guest (guestNo, guestName, guestAddress)
-VALUES ('1735', 'Steven White', 'Coral Gables');
+VALUES (1735, 'Steven White', 'Coral Gables');
 
 INSERT INTO Guest (guestNo, guestName, guestAddress)
-VALUES ('1736', 'Adam Jones', 'Miami');
+VALUES (1736, 'Adam Jones', 'Miami');
 
 INSERT INTO Guest (guestNo, guestName, guestAddress)
-VALUES ('1737', 'Alex Baker', 'Miami');
+VALUES (1737, 'Alex Baker', 'Miami');
 
 INSERT INTO Guest (guestNo, guestName, guestAddress)
-VALUES ('1738', 'Emma Johnson', 'Miami');
+VALUES (1738, 'Emma Johnson', 'Miami');
 
 -- Booking values
 INSERT INTO Booking (hotelNo, guestNo, dateFrom, dateTo, roomNo)
-VALUES ('1002', '1734', DATE'2021-03-15', DATE'2021-03-18', '213');
+VALUES (1002, 1734, DATE'2021-03-15', DATE'2021-03-18', 213);
 
 INSERT INTO Booking (hotelNo, guestNo, dateFrom, dateTo, roomNo)
-VALUES ('1002', '1735', DATE'2021-03-18', DATE'2021-03-21', '403');
+VALUES (1002, 1735, DATE'2021-03-18', DATE'2021-03-21', 403);
 
 INSERT INTO Booking (hotelNo, guestNo, dateFrom, dateTo, roomNo)
-VALUES ('1002', '1736', DATE'2021-03-17', DATE'2021-03-18', '701');
+VALUES (1002, 1736, DATE'2021-03-17', DATE'2021-03-18', 701);
 
 INSERT INTO Booking (hotelNo, guestNo, dateFrom, dateTo, roomNo)
-VALUES ('1002', '1737', DATE'2021-03-25', DATE'2021-03-27', '516');
+VALUES (1002, 1737, DATE'2021-03-25', DATE'2021-03-27', 516);
 
 INSERT INTO Booking (hotelNo, guestNo, dateFrom, dateTo, roomNo)
-VALUES ('1003', '1738', DATE'2021-03-25', DATE'2021-03-30', '307');
+VALUES (1003, 1738, DATE'2021-03-25', DATE'2021-03-30', 307);
 
