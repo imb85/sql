28d70789489aae8c734256711328853de677a1a9
Andrew Kraynak <abk89@miami.edu>
2021-03-27 17:27:01 -0400

    Added search statements

--- hotels/search_tables.sql
+++ hotels/search_tables.sql
@@ -4,7 +4,12 @@ FROM Hotel
 WHERE city='Seattle';
 
 -- List roomNo, roomType, and price of the rooms in the Biltmore Hotel
-
+SELECT roomNo, roomType, price
+FROM Room
+WHERE hotelNo=(
+	SELECT hotelNo
+	FROM Hotel
+	WHERE hotelName='Biltmore Hotel');
 
 -- List the guestNo and guestName of guests who live in Miami alphabetically
 SELECT guestNo, guestName
@@ -12,3 +17,4 @@ FROM Guest
 WHERE guestAddress='Miami';
 
 -- List the number of rooms in each hotel
+
