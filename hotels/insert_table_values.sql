98590d00fe48caaccec0da481cdf27dfb2ca8341
Andrew Kraynak <abk89@miami.edu>
2021-03-27 16:18:30 -0400

    Added values to insert

--- hotels/insert_table_values.sql
+++ hotels/insert_table_values.sql
@@ -1,10 +1,36 @@
 -- Hotel values
+INSERT INTO Hotel (hotelNo, hotelName, city)
+VALUES ('1001', 'Holiday Inn', 'Miami');
 
+INSERT INTO Hotel (hotelNo, hotelName, city)
+VALUES ('1002', 'Biltmore Hotel', 'Coral Gables');
+
+INSERT INTO Hotel (hotelNo, hotelName, city)
+VALUES ('1003', 'Hilton', 'Seattle');
+
+INSERT INTO Hotel (hotelNo, hotelName, city)
+VALUES ('1004', 'Double Tree', 'Chicago');
+
+INSERT INTO Hotel(hotelNo, hotelName, city)
+VALUES ('1005', 'Quality Inn', 'Seattle');
 
 -- Room values
+INSERT INTO Room (roomNo, hotelNo, roomType, price)
+VALUES ('213', '1002', 'Suite', '299.99');
+
+INSERT INTO Room (rooomNo, hotelNo, roomType, price)
+VALUES ('403', '1002', 'Double', '359.99');
+
+INSERT INTO Room (roomNo, hotelNo, roomType, price)
+VALUES ('701', '1002', 'Penthouse', '949.99');
+
+INSERT INTO Room (roomNo, hotelNo, roomType, price)
+VALUES ('516', '1002', 'Double', '369.99');
+
+INSERT INTO Room (roomNo, hotelNo, roomType, price)
+VALUES ('307', '1003', 'Suite', '229.99');
+
+-- Guest values
 
 
 -- Booking values
-
-
--- Guest values
